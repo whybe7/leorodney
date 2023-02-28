c412e817028864ca179774d33e06f72c5cd42a7b
whybe7 <whybe7@hotmail.com>
2023-02-28 23:41:09 +0100

    showcases community pluged in

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,12 @@
 
-export default function Card() {
+export default function Card({author, value, img}) {
   return (
-    <div>Card</div>
+    <div className="relative group flex justify-center items-center rounded-md overflow-hidden">
+      <img src={img} alt={value} className="w-full h-full object-cover"/>
+      <p className="absolute p-3 bottom-[-120%] group-hover:bottom-0 transition-all w-full bg-slate-200 text-gray-900">
+        <span className="font-bold block underline text-xl mb-2 text-black">{author}</span>
+        {value}
+      </p>
+    </div>
   )
 }
--- client/src/components/Community.jsx
+++ client/src/components/Community.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Cards from "./Cards";
 import Loader from "./Loader";
 
 export default function Community({search}) {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [prompts, setPrompts] = useState([]);
+
+    useEffect(()=>{
+      (async ()=>{
+        try {
+          setLoading(true);
+          const response = await axios.get(`${import.meta.env.VITE_SERVER_HOOK_API}/community`);
+          setPrompts(response.data);
+          console.log(response.data);
+        } catch (error) {
+          console.error(error)
+        }finally{
+          setLoading(false)
+        }
+      })()
+    },[]);
     
   return (
     <section className="py-5 px-5 md:px-3 md:py-3 sm:py-1 sm:px-1">
@@ -11,7 +28,7 @@ export default function Community({search}) {
         { loading ? 
         <div className="h-full w-full flex justify-center items-center"> <div className="h-[40px] w-[40px]"><Loader/></div></div> :
         <>
-        {search ? <Cards cards={[]} notFound="No search results found..!"/> : <Cards cards={[]} notFound="No posts found..!"/>}
+        {search ? <Cards cards={prompts} notFound="No search results found..!"/> : <Cards cards={prompts} notFound="No prompts found..!"/>}
         </>
         }
     </section>
