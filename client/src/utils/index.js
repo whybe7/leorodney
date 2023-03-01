54fd55e9edbf7646a0948de2d31c9cb34243cb72
whybe7 <whybe7@hotmail.com>
2023-03-01 23:33:49 +0100

    add download for community prompts

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,11 @@
 import { surpriseMePrompts } from "../constants";
+import { saveAs } from "file-saver";
 
 export function getRandomPrompt(prompt){
     const randomPrompt = surpriseMePrompts[Math.floor(Math.random() * surpriseMePrompts.length)];
     return randomPrompt === prompt ? getRandomPrompt(prompt) : randomPrompt;
 }
+
+export async function downloadPomptImg(name, imgPrompt){  
+    saveAs(imgPrompt, name)
+} 
