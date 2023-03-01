02e4c3cd6a4a7eedca6c22c382f8dab2e9db273a
whybe7 <whybe7@hotmail.com>
2023-03-01 23:37:55 +0100

    load the prompt in the home not in the community

--- client/src/components/Community.jsx
+++ client/src/components/Community.jsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import Cards from "./Cards";
 import Loader from "./Loader";
 
-export default function Community({search}) {
-    const [loading, setLoading] = useState(false);
-    const [prompts, setPrompts] = useState([]);
-
-    useEffect(()=>{
-      (async ()=>{
-        try {
-          setLoading(true);
-          const response = await axios.get(`${import.meta.env.VITE_SERVER_HOOK_API}/community`);
-          setPrompts(response.data);
-          console.log(response.data);
-        } catch (error) {
-          console.error(error)
-        }finally{
-          setLoading(false)
-        }
-      })()
-    },[]);
+export default function Community({search, loading, prompts}) {
     
   return (
     <section className="py-5 px-5 md:px-3 md:py-3 sm:py-1 sm:px-1">
-        { search ? <h1 className="text-3xl font-bold mt-2">Community show case for: <span className="text-[var(--blue-clr)]">"{search}"</span></h1> : <h1 className="text-3xl font-bold mt-2">The Community show cases:</h1> }
-        { loading ? 
-        <div className="h-full w-full flex justify-center items-center"> <div className="h-[40px] w-[40px]"><Loader/></div></div> :
+        { search ? <h1 className="text-3xl font-bold my-4 ml-3">Community show case for: <span className="text-[var(--blue-clr)]">"{search}"</span></h1> : <h1 className="text-3xl font-bold my-4 ml-3">The Community show cases:</h1> }
+        { loading ? <div className="h-full w-full flex justify-center items-center"> <div className="h-[40px] w-[40px]"><Loader/></div></div> :
         <>
         {search ? <Cards cards={prompts} notFound="No search results found..!"/> : <Cards cards={prompts} notFound="No prompts found..!"/>}
         </>
--- client/src/components/Prod.jsx
+++ client/src/components/Prod.jsx
@@ -25,7 +25,6 @@ export default function Prod() {
     if(!prompt.value){ return console.error("Prompt Must not be empty") }
     try {
       setGenerating(true);
-      // const response = await axios.post(`${import.meta.env.VITE_SERVER_HOOK_API}/leorodney`, prompt);
       const response = await axios.post(`${import.meta.env.VITE_SERVER_HOOK_API}/leorodney`, {prompt: prompt.value});
       setPrompt({...prompt,  img: `data:image/png;base64,${response.data.img}`});
     } catch (error) {
